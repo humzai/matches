361812c1ff9906cd47a331957ba448a62fce68d7
Humza <humzai@hotmail.com>
2014-04-14 14:41:25 -0700

    Initial Commit

---
+++ MatchedUp/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  MatchedUp
+//
+//  Created by Humza Iqbal on 4/14/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MatchedUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MatchedUp
+//
+//  Created by Humza Iqbal on 4/14/14.
+//  Copyright (c) 2014 Humza Iqbal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
